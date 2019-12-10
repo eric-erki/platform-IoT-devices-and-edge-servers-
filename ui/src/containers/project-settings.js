1c0e7cf3fc2a053dbb3006da6d6f5ffb0baa2010
raboid <rabautse@gmail.com>
2019-12-09 21:30:43 -0800

    update delete project

--- ui/src/containers/project-settings.js
+++ ui/src/containers/project-settings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import useForm from 'react-hook-form';
 import { useNavigation } from 'react-navi';
-import { Alert, Dialog, toaster } from 'evergreen-ui';
+import { Alert, toaster } from 'evergreen-ui';
 import * as yup from 'yup';
 
 import api from '../api';
@@ -9,6 +9,7 @@ import utils from '../utils';
 import Layout from '../components/layout';
 import Card from '../components/card';
 import Field from '../components/field';
+import Dialog from '../components/dialog';
 import { Text, Row, Button, Form, Input } from '../components/core';
 
 const validationSchema = {
@@ -48,7 +49,8 @@ const ProjectSettings = ({
       });
   };
 
-  const submitDelete = () => {
+  const submitDelete = e => {
+    e.preventDefault();
     setBackendError(null);
     api
       .deleteProject({ projectId: params.project })
@@ -71,44 +73,45 @@ const ProjectSettings = ({
 
   return (
     <Layout alignItems="center">
-      <Card title="Project Settings">
-        <Form onSubmit={handleSubmit(submit)}>
-          {backendError && (
-            <Alert
-              marginBottom={16}
-              paddingTop={16}
-              paddingBottom={16}
-              intent="warning"
-              title={backendError}
+      <>
+        <Card title="Project Settings">
+          <Form onSubmit={handleSubmit(submit)}>
+            {backendError && (
+              <Alert
+                marginBottom={16}
+                paddingTop={16}
+                paddingBottom={16}
+                intent="warning"
+                title={backendError}
+              />
+            )}
+            <Field
+              required
+              label="Name"
+              name="name"
+              ref={register}
+              errors={errors.name}
             />
-          )}
-          <Field
-            required
-            label="Name"
-            name="name"
-            ref={register}
-            errors={errors.name}
-          />
-          <Field
-            label="Datadog API Key"
-            name="datadogApiKey"
-            ref={register}
-            errors={errors.datadogApiKey}
-          />
-          <Button
-            type="submit"
-            title="Update Settings"
-            disabled={!formState.dirty}
-          />
-        </Form>
-        <Row marginTop={4}>
-          <Button
-            title="Delete Project"
-            variant="tertiary"
-            onClick={() => setShowDeleteDialog(true)}
-          />
-        </Row>
-
+            <Field
+              label="Datadog API Key"
+              name="datadogApiKey"
+              ref={register}
+              errors={errors.datadogApiKey}
+            />
+            <Button
+              type="submit"
+              title="Update Settings"
+              disabled={!formState.dirty}
+            />
+          </Form>
+          <Row marginTop={4}>
+            <Button
+              title="Delete Project"
+              variant="tertiary"
+              onClick={() => setShowDeleteDialog(true)}
+            />
+          </Row>
+        </Card>
         <Dialog
           show={showDeleteDialog}
           onClose={() => setShowDeleteDialog(false)}
@@ -119,11 +122,23 @@ const ProjectSettings = ({
               permanently delete the <strong>{params.project}</strong> project.
               <p></p>Please type in the name of the project to confirm.
             </Text>
+            <Form onSubmit={submitDelete}>
+              <Input
+                marginTop={4}
+                placeholder="Project name"
+                onChange={e => setConfirmation(e.target.value)}
+                value={confirmation}
+              />
+              <Button
+                marginTop={4}
+                type="submit"
+                title="Delete Project"
+                disabled={confirmation !== project.name}
+              />
+            </Form>
           </Card>
-
-          <Input onChange={e => setConfirmation(e.target.value)} />
         </Dialog>
-      </Card>
+      </>
     </Layout>
   );
 };
