5b38d9795ff4c96cc3d7bce124b5afe0dc463eb0
raboid <rabautse@gmail.com>
2019-12-09 22:56:11 -0800

    wrapping up

--- ui/src/containers/device/overview.js
+++ ui/src/containers/device/overview.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Table, Badge } from 'evergreen-ui';
 
 import config from '../../config';
 import Card from '../../components/card';
-import { Button } from '../../components/core';
+import { Row, Column, Text, Button } from '../../components/core';
 import { EditableLabelTable } from '../../components/EditableLabelTable';
 
 const checkServices = applicationStatusInfo => {
@@ -19,6 +19,32 @@ const checkServices = applicationStatusInfo => {
 };
 
 const DeviceServices = ({ project, applicationStatusInfo }) => {
+  // const columns = useMemo(
+  //   () => [
+  //     { Header: 'Service', accessor: 'user.email' },
+  //     {
+  //       Header: 'Current Release',
+  //       Cell: ({
+  //         row: {
+  //           original: {
+  //             user: { firstName, lastName },
+  //           },
+  //         },
+  //       }) => `${firstName} ${lastName}`,
+  //     },
+  //     {
+  //       Header: 'Roles',
+  //       Cell: ({
+  //         row: {
+  //           original: { roles },
+  //         },
+  //       }) => roles.map(({ name }) => name).join(', '),
+  //     },
+  //   ],
+  //   []
+  // );
+  // const tableData = useMemo(() => members, [members]);
+
   if (!checkServices(applicationStatusInfo)) {
     return null;
   }
@@ -55,57 +81,57 @@ const DeviceOverview = ({
   },
 }) => {
   return (
-    <>
-      <Card title="Device Info" size="large">
-        <Table>
-          <Table.Head>
-            <Table.TextHeaderCell flexBasis={90} flexShrink={0} flexGrow={0}>
-              Status
-            </Table.TextHeaderCell>
-            <Table.TextHeaderCell>IP Address</Table.TextHeaderCell>
-            <Table.TextHeaderCell>OS</Table.TextHeaderCell>
-          </Table.Head>
-          <Table.Body>
-            <Table.Row>
-              <Table.TextCell
-                flexBasis={90}
-                flexShrink={0}
-                flexGrow={0}
-                alignItems="center"
-                paddingRight="0"
-              >
-                {device.status === 'offline' ? (
-                  <Badge color="red">offline</Badge>
-                ) : (
-                  <Badge color="green">online</Badge>
-                )}
-              </Table.TextCell>
-              <Table.TextCell>
-                {device.info.hasOwnProperty('ipAddress')
-                  ? device.info.ipAddress
-                  : ''}
-              </Table.TextCell>
-              <Table.TextCell>
-                {device.info.hasOwnProperty('osRelease') &&
-                device.info.osRelease.hasOwnProperty('prettyName')
-                  ? device.info.osRelease.prettyName
-                  : '-'}
-              </Table.TextCell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
-      </Card>
-      <Card title="Labels" size="large">
+    <Card size="large">
+      <Row
+        alignItems="flex-end"
+        justifyContent="space-between"
+        marginBottom={5}
+      >
+        <Text fontSize={5} fontWeight={3}>
+          {device.name}
+        </Text>
+        {device.status === 'offline' ? (
+          <Badge color="red">offline</Badge>
+        ) : (
+          <Badge color="green">online</Badge>
+        )}
+      </Row>
+      <Column marginBottom={4}>
+        <Text fontWeight={3} marginBottom={2}>
+          IP Address
+        </Text>
+        <Text>
+          {device.info.hasOwnProperty('ipAddress') ? device.info.ipAddress : ''}
+        </Text>
+      </Column>
+      <Column marginBottom={4}>
+        <Text fontWeight={3} marginBottom={2}>
+          Operating System
+        </Text>
+        <Text>
+          {device.info.hasOwnProperty('osRelease') &&
+          device.info.osRelease.hasOwnProperty('prettyName')
+            ? device.info.osRelease.prettyName
+            : '-'}
+        </Text>
+      </Column>
+      <Column marginBottom={4}>
+        <Text fontWeight={3} marginBottom={2}>
+          Labels
+        </Text>
         <EditableLabelTable
           getEndpoint={`${config.endpoint}/projects/${params.project}/devices/${device.id}`}
           setEndpoint={`${config.endpoint}/projects/${params.project}/devices/${device.id}/labels`}
           deleteEndpoint={`${config.endpoint}/projects/${params.project}/devices/${device.id}/labels`}
         />
-      </Card>
-      <Card title="Services" size="large">
+      </Column>
+      <Column>
+        <Text fontWeight={3} marginBottom={2}>
+          Services
+        </Text>
         <DeviceServices project={params.project} device={device} />
-      </Card>
-    </>
+      </Column>
+    </Card>
   );
 };
 
