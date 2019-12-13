8d591ee68ee66e2cc50e0e9978dd9c02e94fc24f
raboid <rabautse@gmail.com>
2019-12-12 16:42:30 -0800

    value placeholder

--- ui/src/components/core/value.js
+++ ui/src/components/core/value.js
@@ -1,14 +1,21 @@
+import React from 'react';
 import styled from 'styled-components';
 import { space, color, typography } from 'styled-system';
 
-const Value = styled.span`
+const StyledValue = styled.span`
     word-wrap: break-word;
+    min-height: 19px;
     ${color} ${space} ${typography}
 `;
 
-Value.defaultProps = {
+StyledValue.defaultProps = {
   color: 'grays.8',
   fontSize: 2,
+};
+
+const Value = ({ children }) => {
+  const value = children || '-';
+  return <StyledValue>{value}</StyledValue>;
 };
 
 export default Value;
