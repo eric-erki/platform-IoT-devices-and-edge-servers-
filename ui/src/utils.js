adbf47dc918e893dbcde3f33928d7f644b7c91c3
raboid <rabautse@gmail.com>
2019-12-12 18:48:56 -0800

    fix utils

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,26 +1,31 @@
 const uuidv4 = require('uuid/v4');
 
+const capitalize = message => message.replace(/^\w/, c => c.toUpperCase());
+
+const randomClassName = () => {
+  return (
+    'rcn_' +
+    uuidv4()
+      .replace(/-/g, '')
+      .substring(0, 10)
+  );
+};
+
+const deepClone = object => {
+  return JSON.parse(JSON.stringify(object));
+};
+
+const parseError = ({ response: { status, data } }) => {
+  if (data) {
+    return capitalize(data);
+  }
+
+  return null;
+};
+
 export default {
-  randomClassName: () => {
-    return (
-      'rcn_' +
-      uuidv4()
-        .replace(/-/g, '')
-        .substring(0, 10)
-    );
-  },
-
-  deepClone: object => {
-    return JSON.parse(JSON.stringify(object));
-  },
-
-  capitalize: message => message.replace(/^\w/, c => c.toUpperCase()),
-
-  parseError: ({ response: { status, data } }) => {
-    if (data) {
-      return this.capitalize(data);
-    }
-
-    return null;
-  },
+  randomClassName,
+  deepClone,
+  capitalize,
+  parseError,
 };
